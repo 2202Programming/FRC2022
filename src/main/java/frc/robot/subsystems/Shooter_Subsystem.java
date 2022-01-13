03a3789e7f928a5534a964a7662a16713636afcd
Sarah <22rubens@elmbrookstudents.org>
2022-01-12 19:28:35 -0600

    Skeleton shooter

---
+++ src/main/java/frc/robot/subsystems/Shooter_Subsystem.java
@@ -0,0 +1,59 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter_Subsystem extends SubsystemBase {
+  /** Creates a new Shooter_Subsystem. */
+  public Shooter_Subsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setMotorSpeed(double speed){
+    //sets motor speed to go speed ft/s
+  }
+
+  //takes angle class (has vertical and horizontal)
+  public void setAngle(Angle angle){
+    //sets angles to angles designated by Angle class 
+  }
+
+  //Could return a Motor speed object
+  public double getMotorSpeed(){
+    //return speed of a motor in ft/s
+  }
+
+  public Angle getAngle(){
+    //returns an angle class with the angle of the shooter 
+  }
+
+  public boolean readyToShoot(){
+    //returns wether or not the robot is in position to shoot (correct angle, etc.)
+  }
+
+  public void shoot(){
+    //shoots the ball
+  }
+
+  public class Angle(){
+    double verticalAngle;
+    double horizontalAngle; 
+
+    public Angle(){
+
+    }
+
+    public double getVerticalAngle(){
+      
+    }
+
+
+  }
+
+}
