e6c6516cb4d4431523e6c3e8af627c2303b3ef79
Derek <derek@adminnet.us>
2022-01-23 09:42:02 -0600

    Restored PID initialization, added notes/todo

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -11,72 +11,83 @@ public class Climber {
     // PIDSlot used
     int slot = 0;
 
-    //raise/lower controllers
+    // raise/lower controllers
     private CANSparkMax li_extender = new CANSparkMax(CAN.CMB_LI_Extend, MotorType.kBrushless);
     private CANSparkMax lo_extender = new CANSparkMax(CAN.CMB_LO_Extend, MotorType.kBrushless);
     private CANSparkMax ri_extender = new CANSparkMax(CAN.CMB_RI_Extend, MotorType.kBrushless);
     private CANSparkMax ro_extender = new CANSparkMax(CAN.CMB_RO_Extend, MotorType.kBrushless);
 
-    //rotation arm controller (outer arms rotate)
+    // rotation arm controller (outer arms rotate)
     private CANSparkMax l_rotator = new CANSparkMax(CAN.CMB_L_Rotate, MotorType.kBrushless);
     private CANSparkMax r_rotator = new CANSparkMax(CAN.CMB_R_Rotate, MotorType.kBrushless);
 
-  
     public Climber() {
-        raise1 = new CANSparkMax(Constants.CAN.RAISE1, MotorType.kBrushless);
-        lower1 = new CANSparkMax(Constants.CAN.LOWER1, MotorType.kBrushless);
-        raise2 = new CANSparkMax(Constants.CAN.RAISE2, MotorType.kBrushless);
-        lower2 = new CANSparkMax(Constants.CAN.LOWER2, MotorType.kBrushless);
+        // TODO - raise/lower are actions ie commands. In the sub-sys we are defining
+        // devices
+        // and behaviors. Left/Right Inner/Outer arms left-rotator, right-rotator
+        // gives a better idea of what
 
-        angle1 = new CANSparkMax(Constants.CAN.ANGLE1, MotorType.kBrushless);
-        angle2 = new CANSparkMax(Constants.CAN.ANGLE2, MotorType.kBrushless);
+        // TODO Set the control type and PID settings - most likely position control
+
+        // Copy the PID settings down to the SparkMax hardware 
+        // Rotation PIDS
+        ClimbSettings.armPID.copyTo(l_rotator.getPIDController(), slot);
+        ClimbSettings.armPID.copyTo(r_rotator.getPIDController(), slot);
+        // Arm extension PIDS
+        ClimbSettings.innerPID.copyTo(li_extender.getPIDController(), slot);
+        ClimbSettings.innerPID.copyTo(ri_extender.getPIDController(), slot);
+        ClimbSettings.outerPID.copyTo(lo_extender.getPIDController(), slot);
+        ClimbSettings.outerPID.copyTo(ro_extender.getPIDController(), slot);
 
     }
 
-    public boolean readyToClimb(){
-        //returns if robot is in the right position, and all motors are in place
+    public boolean readyToClimb() {
+        // returns if robot is in the right position, and all motors are in place
         return false;
     }
 
-    public String rung(){
+    public String rung() {
         return "blank";
     }
 
-    public boolean readyToTraverse(){
+    public boolean readyToTraverse() {
         return false;
     }
 
     /**
      * Left and Right arms are controlled in pairs
-     *      L/R Inner arms move together
-     *      L/R Outer arms move together
+     * L/R Inner arms move together
+     * L/R Outer arms move together
+     * 
      * @param inches from retracted position
      */
-    //for raising/lower
-    public void setInnerExtension(double inches){
-        //negative values lower
-        //assumed vertical 
+    // for raising/lower
+    public void setInnerExtension(double inches) {
+        // negative values lower
+        // assumed vertical
     }
-    
+
     /**
      * Left and Right arms are controlled in pairs
-     *      L/R Outer arms move together
+     * L/R Outer arms move together
+     * 
      * @param inches from retracted position
      */
-    public void setOuterExtension(double inches){
-    //negative values lower
-    //assumed vertical 
-}
+    public void setOuterExtension(double inches) {
+        // negative values lower
+        // assumed vertical
+    }
+
     /**
      * Outer L/R arms rotate together
      * 
-     * @param degrees  +/- degrees from vertical
+     * @param degrees +/- degrees from vertical
      */
-    public void setRotation(double degrees){
-        //changes the angle of the ? by this many degrees
+    public void setRotation(double degrees) {
+        // changes the angle of the ? by this many degrees
         l_rotator.getPIDController().setReference(degrees, ControlType.kPosition);
         r_rotator.getPIDController().setReference(degrees, ControlType.kPosition);
 
-    }    
+    }
 
 }
