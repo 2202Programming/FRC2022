dc98ac45af026fff453b8a19b6b2efd3db8d83b0
Nathanael Ren <nren2013@gmail.com>
2022-02-28 19:37:30 -0600

    added generic command with inputs for desired rotation, extension
    Not implemented anywhere

---
+++ src/main/java/frc/robot/commands/GenericClimbCmd.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+import frc.robot.Constants;
+
+public class GenericClimbCmd extends CommandBase {
+
+    private final Climber climber;
+
+    // desired extension and rotation positions
+    private double ext_pos;
+    private double rot_pos;
+    
+    public GenericClimbCmd(Climber climber, double ext_pos, double rot_pos) {
+        this.climber = climber;
+        this.ext_pos = ext_pos;
+        this.rot_pos = rot_pos;
+    }
+    
+    @Override
+    public void initialize() {
+        climber.setAmperageLimit(Constants.ClimbSettings.MAX_AMPERAGE);
+    }
+
+    @Override
+    public void execute() {
+        climber.setExtension(ext_pos);
+        climber.setRotation(rot_pos);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return climber.checkIsFinished(ext_pos, rot_pos);
+    }
+
+
+}
