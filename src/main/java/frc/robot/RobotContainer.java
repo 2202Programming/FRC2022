cc3c9ab4ad291fb73dfad55f9c9fffb7754205cd
Beast Software <beastsoftware2202@gmail.com>
2022-03-05 11:27:01 -0600

    Why did the Assistant have this?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -181,9 +181,7 @@ public class RobotContainer {
 
     if(Constants.HAS_SHOOTER){
       driverControls.bind(Id.Assistant, XboxAxis.TRIGGER_RIGHT).whileHeld(new BasicShootCommand(new ShooterSettings(20, 0.0), 20 ));
-    }  
-       // Y - reset Pose
-       if (Constants.HAS_DRIVETRAIN) driverControls.bind(Id.Assistant, XboxButton.Y).whenPressed(new InstantCommand(drivetrain::resetPose));
+    }
 
   }
 
