6ab2ac8f9fd0267a3db809b3d1cdb0b6bc804d90
Kamith <kvml96@gmail.com>
2022-03-05 10:07:47 -0600

    more stuff

--- src/main/java/frc/robot/subsystems/trainingShooter/Test_FlyWheelRPM.java
+++ src/main/java/frc/robot/subsystems/trainingShooter/Test_FlyWheelRPM.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems.trainingShooter;
 
-public class FlyWheelRPM {
-    
+public class Test_FlyWheelRPM {
+    public double upper;
+    public double lower;
+
+    //create an empty constructor
+    public FlyWheelRPM(){} 
+
+    //create a constructor where we pass in a FlyWheelRPM
+    public FlyWheelRPM() }
+
+    //create a constructor where we pass in two values for upper and lower
+    public FlyWheelRPM() {}
+  
+
+    //create a function where we can return an average of upper and lower
+
+    //create a function to copy new FlywheelRPM
+    public FlyWheelRPM copy(FlyWheelRPM src) {}
+
+    //create a function to set new RPM
+    public FlyWheelRPM set(){}
+
+    //create a function to minus 2 flywheelrpm
+
+    //create a toString()
+
 }
