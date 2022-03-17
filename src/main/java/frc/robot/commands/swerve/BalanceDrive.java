f999722fc615d147e9462f448ff4826c1cd7e43f
Jason <jruben4@yahoo.com>
2022-03-17 12:58:48 -0500

    wip

--- src/main/java/frc/robot/commands/swerve/BalanceDrive.java
+++ src/main/java/frc/robot/commands/swerve/BalanceDrive.java
@@ -1,25 +1,13 @@
 package frc.robot.commands.swerve;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.Constants.DriveTrain;
 import frc.robot.subsystems.SwerveDrivetrain;
-import frc.robot.subsystems.ifx.DriverControls;
 
-/* Current driving behavior:
-  Starts in field centric
-  B will toggle between field centric and intake centric
-  Holding right trigger will switch to hub centric until you let go, then it will go back to original mode
-          (either field or intake centric, depending what you started in)
-  If in intake centric and you try to rotate with left joystick, will drop back to field centric mode.
-*/
-
+//Autobalance to correct tipping
 
 public class BalanceDrive extends CommandBase {
 
@@ -30,7 +18,9 @@ public class BalanceDrive extends CommandBase {
   double xSpeed, ySpeed, rot;
   SwerveModuleState[] output_states;
 
-  double log_counter = 0;
+  //probably inverted to go opposite way of tip?
+  final static double kPitchStrengthCorrectionFactor = -1.0;
+  final static double kRollStrengthCorrectionFactor = -1.0;
 
   public BalanceDrive(SwerveDrivetrain drivetrain) {
     this.drivetrain = drivetrain;
@@ -45,8 +35,8 @@ public class BalanceDrive extends CommandBase {
 
   void calculate() {
     // Magnatude of roll or pitch correction dependant on degree of tip
-    xSpeed = Math.sin(Math.toRadians(RobotContainer.RC().sensors.getAHRS().getPitch())) * -1.0;
-    ySpeed = Math.sin(Math.toRadians(RobotContainer.RC().sensors.getAHRS().getRoll())) * -1.0;
+    xSpeed = Math.sin(Math.toRadians(RobotContainer.RC().sensors.getAHRS().getPitch())) * kPitchStrengthCorrectionFactor;
+    ySpeed = Math.sin(Math.toRadians(RobotContainer.RC().sensors.getAHRS().getRoll())) * kRollStrengthCorrectionFactor;
     rot = 0;
     output_states = kinematics.toSwerveModuleStates(new ChassisSpeeds(xSpeed, ySpeed, rot));
   }
