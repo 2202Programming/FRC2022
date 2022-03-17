3c44dedc582297da5143d8f4d6b448ad50d7c9b3
Derek <derek@adminnet.us>
2022-03-16 19:53:58 -0500

    duh!!!!

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -105,7 +105,7 @@ public class Climber extends SubsystemBase {
      * @param rot_spd [deg/s]
      */
     public void setRotSpeed(double rot_spd) { 
-        setExtSpeed(rot_spd, rot_spd);
+        setRotSpeed(rot_spd, rot_spd);
     }
     public void setRotSpeed(double rot_spd_lt, double rot_spd_rt) {
         double rt_comp = (sync_arms) ? rot_compensation : 0.0 ;
