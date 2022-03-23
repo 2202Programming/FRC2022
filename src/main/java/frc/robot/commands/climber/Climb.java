bcad90081b450035f920e543e926e93e4640678d
Tom Lazar <me@tomlazar.net>
2022-03-22 19:14:04 -0500

    add kill switch

--- src/main/java/frc/robot/commands/climber/Climb.java
+++ src/main/java/frc/robot/commands/climber/Climb.java
@@ -1,17 +1,27 @@
 package frc.robot.commands.climber;
 
-import frc.robot.subsystems.SwerveDrivetrain;
-import frc.robot.subsystems.climber.Climber;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.ClimbSettings;
+import frc.robot.subsystems.SwerveDrivetrain;
+import frc.robot.subsystems.climber.Climber;
+import frc.robot.subsystems.hid.SideboardController.SBButton;
+import frc.robot.subsystems.hid.XboxButton;
+import frc.robot.subsystems.ifx.DriverControls.Id;
 
 public class Climb extends SequentialCommandGroup {
     // define amount of time to wait for robot to stop swinging between steps - edit to tune
     private final int mid_stabilize_seconds = 3;
     private final int high_stabilize_seconds = 5;
+    private Climber climber;
+
+    private LinearFilter filter;
 
     public Climb(Climber climber, SwerveDrivetrain drivetrain) {
         super();
+        this.climber = climber;
         // Use the following subcommands to climb climbMID climbHIGH stabilize
         this.addCommands(
             //TODO:  add drivetrain stuff
@@ -23,5 +33,43 @@ public class Climb extends SequentialCommandGroup {
         );
 
         this.addRequirements(climber, drivetrain);
+        filter = LinearFilter.movingAverage(3);
+
+    }
+
+
+
+
+    @Override
+    public boolean isFinished() {
+        var doKill = false;
+        if(RobotContainer.RC().driverControls.bind(Id.SwitchBoard, SBButton.Sw11).get()) {
+            System.out.println("** CLIMBER KILLED reason=sb-button");
+            doKill = true;
+        }
+
+        if(RobotContainer.RC().driverControls.bind(Id.Driver, XboxButton.START).get()) {
+            System.out.println("** CLIMBER KILLED reason=driver-button");
+            doKill = true;
+        }
+
+        if(RobotContainer.RC().driverControls.bind(Id.Assistant, XboxButton.START).get()) {
+            System.out.println("** CLIMBER KILLED reason=assistant-button");
+            doKill = true;
+        }
+
+        var anglediff =filter.calculate(Math.abs(climber.getLeftRotation() - climber.getRightRotation()));
+        if(anglediff > ClimbSettings.KILL_COUNT) {
+            System.out.println("** CLIMBER KILLED reason=angle-diff anglediff=" + anglediff);
+            doKill = true;
+        }
+
+        if(doKill) {
+            climber.setRotSpeed(0);
+            climber.setExtSpeed(0);
+            return true;
+        }
+
+        return super.isFinished();
     }
 }
