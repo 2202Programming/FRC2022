9972a2d3ca5cb879c8e113e8631b900f2c178d04
Nimai <25nallun@elmbrookstudents.org>
2022-04-02 11:30:11 -0500

    Worked on LED_Subsystem

--- src/main/java/frc/robot/subsystems/LED_Subsystem.java
+++ src/main/java/frc/robot/subsystems/LED_Subsystem.java
@@ -1,5 +1,21 @@
 package frc.robot.subsystems;
 
-public class LED_Subsystem {
+import com.ctre.phoenix.CANifier;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class LED_Subsystem extends SubsystemBase {
+    CANifier _canifier = new CANifier(0);
+    int _loopCount = 0;
     
+    public LED_Subsystem() {
+        
+    }
+    public void teleopPeriodic() {
+        if(_loopCount++ > 10)
+        {
+            _loopCount = 0;
+            System.out.println("Bus voltage is: " + _canifier.getBusVoltage());
+        }
+    }
 }
